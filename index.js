80e3d657722e9b0b9611b814732a213242750d35
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-02-26 20:47:44 +0100

    adding about me section

--- index.js
+++ index.js
@@ -5,15 +5,38 @@
 
 import clear from "clear";
 import chalk from "chalk";
+import boxen from "boxen";
 
 clear();
 
 const data = {
-    name: chalk.bold.green("Sofiane"),
-    work: chalk.white("Work:") + "Developer",
-    github: chalk.gray("GitHub: ") + chalk.green("https://github.com/Sofiane5900"),
+    name: chalk.bold.underline.green("Sofiane"),
+    work: chalk.white("Studying:") + " Application Design and Development",
+    github: chalk.gray("GitHub: ") + chalk.white("https://github.com/Sofiane5900"),
     portfolio: chalk.gray("Portfolio: ") + chalk.cyan("https://portfolio-sofiane.vercel.app/"),
     discord: chalk.gray("Discord: ") + chalk.magenta("sofiane590_"),
     npx: chalk.red("npx") + " " + chalk.white("sofiane")
 }
+
+const aboutMe = boxen([
+    `${data.name}`,
+    ``,
+    `${data.work}`,
+    ``,
+    `${data.github}`,
+    `${data.portfolio}`,
+    `${data.discord}`,
+    `${data.npx}`,
+    ``,
+    `${chalk.italic("Select an option to know more about me !")}`
+].join("\n"), {
+    margin: 1,
+    float: "center",
+    padding: 1,
+    borderStyle: "round",
+    borderColor: "cyan"
+    }
+);
+
+console.log(aboutMe);
 
