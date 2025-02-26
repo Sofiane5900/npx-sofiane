36242ae33c56a447218bfd054dd90d441d9b982b
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-02-26 20:22:08 +0100

    adding data.npx

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ const data = {
     github: chalk.gray("https://github.com/") + chalk.green("Sofiane5900"),
     portfolio: chalk.cyan("https://portfolio-sofiane.vercel.app/"),
     discord: chalk.magenta("sofiane590_"),
+    npx: chalk.red("npx") + " " + chalk.white("sofiane")
 }
 console.log(data.name);
 console.log(data.work);
 console.log(data.github);
 console.log(data.portfolio);
 console.log(data.discord);
+console.log(data.npx);
