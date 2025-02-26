c500f811ee8c552d7176a4932f03f59b6b790fda
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-02-26 20:34:17 +0100

    adding labels

--- index.js
+++ index.js
@@ -3,20 +3,17 @@
 // on utilise /env pour chercher dans le PATH
 
 
-
+import clear from "clear";
 import chalk from "chalk";
+
+clear();
 
 const data = {
     name: chalk.bold.green("Sofiane"),
-    work: chalk.white("Développeur passionné"),
-    github: chalk.gray("https://github.com/") + chalk.green("Sofiane5900"),
-    portfolio: chalk.cyan("https://portfolio-sofiane.vercel.app/"),
-    discord: chalk.magenta("sofiane590_"),
+    work: chalk.white("Work:") + "Developer",
+    github: chalk.gray("GitHub: ") + chalk.green("https://github.com/Sofiane5900"),
+    portfolio: chalk.gray("Portfolio: ") + chalk.cyan("https://portfolio-sofiane.vercel.app/"),
+    discord: chalk.gray("Discord: ") + chalk.magenta("sofiane590_"),
     npx: chalk.red("npx") + " " + chalk.white("sofiane")
 }
-console.log(data.name);
-console.log(data.work);
-console.log(data.github);
-console.log(data.portfolio);
-console.log(data.discord);
-console.log(data.npx);
+
